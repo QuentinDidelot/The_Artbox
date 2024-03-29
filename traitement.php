fbbd12fc8368de1f52bfff875f69e10d70410a52
Quentin D <quentin.didelot13@gmail.com>
2024-03-29 10:27:48 +0100

    Étape 5
    Ajout du htmlspecialchars

--- traitement.php
+++ traitement.php
@@ -2,14 +2,19 @@
     require 'ajouter.php';
 
     if(empty($_POST['titre']) 
-    || empty($_POST['descritpion']) 
-    || empty($_POST['artiste']) 
-    || empty($_POST['image']) 
-    || strlen($_POST['description']) < 3
-    || !filter_var($_POST['image'], FILTER_VALIDATE_URL))
+        || empty($_POST['descritpion']) 
+        || empty($_POST['artiste']) 
+        || empty($_POST['image']) 
+        || strlen($_POST['description']) < 3
+        || !filter_var($_POST['image'], FILTER_VALIDATE_URL)) 
     {
         header('Location : ajouter.php');
     } else {
+        $titre = htmlspecialchars($_POST['titre']);
+        $artiste = htmlspecialchars($_POST['artiste']);
+        $image = htmlspecialchars($_POST['image']);
+        $description = htmlspecialchars($_POST['description']);
+    
         // insérer
     }
 ?>
